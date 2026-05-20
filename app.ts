3d2f90150dc9de5e8afccc61dfb8cafc8c628933
vohoanghaai <vohoangha.ai@gmail.com>
2026-05-20 17:51:00 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -15,7 +15,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const upload = multer({ storage: multer.memoryStorage() });
 
